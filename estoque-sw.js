236020f2e78073798739943dfa78d7dfbee746f8
lfsdesign <leandrofelipearcofoods@gmail.com>
2026-06-24 14:00:20 -0300

    Add files via upload

---
+++ estoque-sw.js
@@ -0,0 +1,17 @@
+var CACHE = 'estoque-pro-v1';
+var FILES = ['./estoque-pro.html'];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(FILES); }));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(r){
+      return r || fetch(e.request).catch(function(){
+        return caches.match('./estoque-pro.html');
+      });
+    })
+  );
+});
